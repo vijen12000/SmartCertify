1200dab613ffeb53057b42928bf59418df2e115d
Vijender Kumar <vijender.kumar@communify.com>
2026-01-21 15:06:43 +0530

    templates for header, footer,and home components

--- UI/smartcertify/src/app/app.ts
+++ UI/smartcertify/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { About } from "./pages/about/about";
+import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
+import { Home } from "./components/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, About, Header, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
